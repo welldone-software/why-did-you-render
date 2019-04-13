e3d7ff6b5cdfa1c1747f33455c9b611c7d108114
vzaidman <vzaidman@gmail.com>
2019-04-13 14:28:09 +0300

    minor demo improvement

--- demo/src/reactRedux/index.js
+++ demo/src/reactRedux/index.js
@@ -11,7 +11,7 @@ Provider = Provider.default
 export default {
   description: 'React Redux',
   fn({domElement, whyDidYouRender}){
-    whyDidYouRender(React)
+    whyDidYouRender(React, {include: [/^ConnectFunction$/]})
 
     const initialState = {a: {b: 'c'}}
 
@@ -48,8 +48,6 @@ export default {
         sameObj: () => ({type: 'sameObj'})
       })
     )(SimpleComponent)
-
-    ConnectedSimpleComponent.whyDidYouRender = true
 
     const Main = () => (
       <Provider store={store}>
