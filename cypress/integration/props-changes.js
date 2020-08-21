baf2eab7ab00146b8aec63979c45778c17d97a67
vzaidman <vzaidman@gmail.com>
2020-08-21 15:27:17 +0300

    added a detailed diff report

--- cypress/integration/props-changes.js
+++ cypress/integration/props-changes.js
@@ -3,9 +3,10 @@ it('props changes', () => {
   cy.wait(100) // eslint-disable-line cypress/no-unnecessary-waiting
   cy.getConsoleSpy(console => {
     expect(console.group).to.be.calledWithMatches([
-      {match: 'ClassDemo', times: 3},
-      {match: 'Rendered by Main', times: 3},
-      {match: /props.*a\W/, times: 4}
+      {match: 'ClassDemo', times: 5},
+      {match: 'Rendered by Main', times: 5},
+      {match: /props.*a\W/, times: 4},
+      {match: /props.*containerProps\W/, times: 4}
     ])
   })
 })
---
+++ src/printDiff.js
@@ -0,0 +1,17 @@
+import {sortBy, groupBy} from 'lodash'
+import calculateDeepEqualDiffs from './calculateDeepEqualDiffs'
+import {diffTypesDescriptions} from './consts'
+
+export default function printDiff(value1, value2, {pathString, consoleLog}){
+  const diffs = calculateDeepEqualDiffs(value1, value2, pathString, {detailed: true})
+
+  const keysLength = Math.max(...diffs.map(diff => diff.pathString.length)) + 2
+
+  Object.entries(groupBy(sortBy(diffs, 'pathString'), 'diffType'))
+    .forEach(([diffType, diffs]) => {
+      consoleLog(`%c${diffTypesDescriptions[diffType]}:`, 'text-decoration: underline; color: blue;')
+      diffs.forEach(diff => {
+        consoleLog(`${diff.pathString}:`.padEnd(keysLength, ' '), diff.prevValue)
+      })
+    })
+}
