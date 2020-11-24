2079a0104749165397459b0452c354ee53e2102c
vzaidman <vzaidman@gmail.com>
2020-11-24 12:35:26 +0200

    improved even more how both react 16 and react 17 are tested

--- jest.config.js
+++ jest.config.js
@@ -10,3 +10,13 @@ module.exports = {
     '^@welldone-software/why-did-you-render$': '<rootDir>/src/whyDidYouRender.js'
   }
 }
+
+if(process.env.USE_REACT_16 === 'true'){
+  module.exports.cacheDirectory = '.cache/jest-cache-react-16'
+  module.exports.moduleNameMapper = {
+    ...module.exports.moduleNameMapper,
+    '^react-is((\\/.*)?)$': 'react-is-16$1',
+    '^react-dom((\\/.*)?)$': 'react-dom-16$1',
+    '^react((\\/.*)?)$': 'react-16$1'
+  }
+}
--- jestSetup.js
+++ jestSetup.js
@@ -4,24 +4,6 @@ import {errorOnConsoleOutput} from '@welldone-software/jest-console-handler'
 
 global.flushConsoleOutput = errorOnConsoleOutput()
 
-if(process.env.USE_REACT_16 === 'true'){
-  jest.mock('react', () => {
-    return jest.requireActual('react-16')
-  })
-
-  jest.mock('react-dom', () => {
-    return jest.requireActual('react-dom-16')
-  })
-
-  jest.mock('react-dom/test-utils', () => {
-    return jest.requireActual('react-dom-16/test-utils')
-  })
-
-  jest.mock('react-is', () => {
-    return jest.requireActual('react-is-16')
-  })
-}
-
 const React = require('react')
 if(process.env.USE_REACT_16 === 'true'){
   if(!React.version.startsWith('16')){
