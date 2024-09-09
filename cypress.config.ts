97e4f8c923d477224d3f4c3a360a043178f870ab
Christian Vuerings <christian@cambly.com>
2024-09-09 03:49:23 -0700

    Use GitHub actions instead of Travis CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress'
+import cypressSplit from 'cypress-split'
 
 export default defineConfig({
   projectId: 'k4cvdh',
@@ -6,6 +7,7 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      cypressSplit(on, config)
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:3003',
