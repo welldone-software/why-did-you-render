1f524c0d8b162e85eaa9b6c0f5dfd45d6431764b
Vitali Zaidman <vzaidman@gmail.com>
2024-09-15 17:58:11 +0100

    fix jest polyfills

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -22,3 +22,21 @@ Object.defineProperties(globalThis, {
   Blob: { value: Blob },
   File: { value: File },
 });
+
+window.MessageChannel = jest.fn().mockImplementation(() => {
+  let onmessage;
+  return {
+    port1: {
+      set onmessage(cb) {
+        onmessage = cb;
+      },
+    },
+    port2: {
+      postMessage: data => {
+        if (onmessage) {
+          onmessage({ data });
+        }
+      },
+    },
+  };
+});
