7325c08f2e17b593d52f057f7f77cbbf75363079
Vitali Zaidman <vzaidman@gmail.com>
2024-12-28 16:55:58 +0000

    fix forward ref tests

--- jestSetup.js
+++ jestSetup.js
@@ -3,7 +3,8 @@ import { errorOnConsoleOutput } from '@welldone-software/jest-console-handler';
 const substringsToIgnore = [
   'Selectors that return the entire state are almost certainly a mistake',
   'Warning: ReactDOM.render is no longer supported in React 19',
-  'Support for defaultProps will be removed from'
+  'Support for defaultProps will be removed from',
+  'forwardRef render functions accept exactly two parameters'
 ];
 const regexToIgnore = new RegExp(`(${substringsToIgnore.join('|')})`);
 
