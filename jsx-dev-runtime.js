395d14dcf2aa295f5daedf32ae32f26da19a58e6
Vitali Zaidman <vzaidman@gmail.com>
2025-01-18 15:50:27 +0000

    fix for react native automatic jsx

--- jsx-dev-runtime.js
+++ jsx-dev-runtime.js
@@ -5,33 +5,35 @@ var WDYR = require('@welldone-software/why-did-you-render')
 var origJsxDev = jsxDevRuntime.jsxDEV
 var wdyrStore = WDYR.wdyrStore
 
-module.exports = jsxDevRuntime
-module.exports.jsxDEV = function jsxDEV(...args){
-  if (wdyrStore.React && wdyrStore.React.__IS_WDYR__) {
-    var origType = args[0]
-    var rest = args.slice(1)
-
-    var WDYRType = WDYR.getWDYRType(origType)
-    if (WDYRType) {
-      try {
-        wdyrStore.ownerBeforeElementCreation = WDYR.getCurrentOwner();
-        var element = origJsxDev.apply(null, [WDYRType].concat(rest))
-        if (wdyrStore.options.logOwnerReasons) {
-          WDYR.storeOwnerData(element)
-        }
-        return element
-      } catch(e) {
-        wdyrStore.options.consoleLog('whyDidYouRender JSX transform error. Please file a bug at https://github.com/welldone-software/why-did-you-render/issues.', {
-          errorInfo: {
-            error: e,
-            componentNameOrComponent: origType,
-            rest: rest,
-            options: wdyrStore.options
+module.exports = {
+  ...jsxDevRuntime,
+  jsxDEV(...args) {
+    if (wdyrStore.React && wdyrStore.React.__IS_WDYR__) {
+      var origType = args[0]
+      var rest = args.slice(1)
+  
+      var WDYRType = WDYR.getWDYRType(origType)
+      if (WDYRType) {
+        try {
+          wdyrStore.ownerBeforeElementCreation = WDYR.getCurrentOwner();
+          var element = origJsxDev.apply(null, [WDYRType].concat(rest))
+          if (wdyrStore.options.logOwnerReasons) {
+            WDYR.storeOwnerData(element)
           }
-        })
+          return element
+        } catch(e) {
+          wdyrStore.options.consoleLog('whyDidYouRender JSX transform error. Please file a bug at https://github.com/welldone-software/why-did-you-render/issues.', {
+            errorInfo: {
+              error: e,
+              componentNameOrComponent: origType,
+              rest: rest,
+              options: wdyrStore.options
+            }
+          })
+        }
       }
     }
+    
+    return origJsxDev.apply(null, args)
   }
-  
-  return origJsxDev.apply(null, args)
-}
+};
